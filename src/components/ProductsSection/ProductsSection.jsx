54434e4d85cf1d1affcb71acdf1ff7bd5eb5eae0
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-10 18:15:17 +0100

    refactor: remove section title from ProductsSection component

--- src/components/ProductsSection/ProductsSection.jsx
+++ src/components/ProductsSection/ProductsSection.jsx
@@ -1,16 +1,13 @@
 import styles from './ProductsSection.module.css';
 import ProductCard from '../ProductCard/ProductCard';
 
-const ProductsSection = ({ title }) => {
+const ProductsSection = () => {
   return (
-    <section className={styles.section}>
-      <h1 className={styles.title}>{title}</h1>
-      <div className={styles.products}>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
+    <section className={styles.products}>
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
     </section>
   );
 };
