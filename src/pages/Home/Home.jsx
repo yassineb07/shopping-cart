1546636a389684251f8fcc2530aa5970de49fade
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-10 15:22:06 +0100

    feat: set up home page structure

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,19 @@
 import styles from './Home.module.css';
+import Header from '../../components/Header/Header';
+import HeroSection from '../../components/HeroSection/HeroSection';
+import ProductsSection from '../../components/ProductsSection/ProductsSection';
+import Announcement from '../../components/Announcement/Announcement';
 
 const Home = () => {
-  return <h1 className={styles.page}>welcome to home</h1>;
+  return (
+    <div className={styles.home}>
+      <Announcement text={'welcome to home'} />
+      <Header />
+      <HeroSection />
+      <ProductsSection title={'Featured Products'} />
+      <ProductsSection title={'Best Sellers'} />
+    </div>
+  );
 };
 
 export default Home;
