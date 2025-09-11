379f3221d8bd6bb02fc768cc9ba6b0ca38ec0cc8
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-11 16:44:40 +0100

    refactor: remove Announcement and Header from store page

--- src/pages/Store/Store.jsx
+++ src/pages/Store/Store.jsx
@@ -1,13 +1,9 @@
 import styles from './Store.module.css';
-import Header from '../../components/Header/Header';
-import Announcement from '../../components/Announcement/Announcement';
 import ProductsSection from '../../components/ProductsSection/ProductsSection';
 
 const Store = () => {
   return (
     <div>
-      <Announcement text={'welcome to store'} />
-      <Header />
       <section className={styles.products}>
         <h1 className={styles.title}>Products</h1>
         <ProductsSection />
