842557664bbeb0e29b0d1a2410259cd4bc8c0642
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-11 16:40:54 +0100

    feat: configure app routes with error handling

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,19 +1,19 @@
+import App from './App';
 import Home from './pages/Home/Home';
 import Store from './pages/Store/Store';
 import Cart from './pages/Cart/Cart';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
 
 const routes = [
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: 'store',
-    element: <Store />,
-  },
-  {
-    path: 'cart',
-    element: <Cart />,
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'store', element: <Store /> },
+      { path: 'cart', element: <Cart /> },
+    ],
   },
 ];
 
