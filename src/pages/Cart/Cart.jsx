fe0adc40514a6a7a8456f917cf4e5b6c9f87c5ee
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-11 14:13:10 +0100

    feat: setup Cart page structure

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,29 @@
 import styles from './Cart.module.css';
+import Header from '../../components/Header/Header';
+import Announcement from '../../components/Announcement/Announcement';
+import ProductPage from '../../components/ProductPage/ProductPage';
 
 const Cart = () => {
-  return <h1 className={styles.page}>welcome to cart</h1>;
+  return (
+    <div>
+      <Announcement text={'welcome to cart'} />
+      <Header />
+      <main className={styles.main}>
+        <h1 className={styles.title}>Cart</h1>
+        <ul className={styles.cart}>
+          <li>
+            <ProductPage />
+          </li>
+          <li>
+            <ProductPage />
+          </li>
+          <li>
+            <ProductPage />
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
 };
 
 export default Cart;
