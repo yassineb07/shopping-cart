52cf8b881fe59559cc8fd4a53f5c22ef81b8cc24
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-11 16:43:51 +0100

    refactor: remove Announcement and Header from home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,10 @@
 import styles from './Home.module.css';
-import Header from '../../components/Header/Header';
 import HeroSection from '../../components/HeroSection/HeroSection';
 import ProductsSection from '../../components/ProductsSection/ProductsSection';
-import Announcement from '../../components/Announcement/Announcement';
 
 const Home = () => {
   return (
     <div className={styles.home}>
-      <Announcement text={'welcome to home'} />
-      <Header />
       <HeroSection />
       <section className={styles.productSection}>
         <h1 className={styles.productSectionTitle}>Featured Products</h1>
