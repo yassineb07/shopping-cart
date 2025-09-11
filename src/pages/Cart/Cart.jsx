4faa82dcd2efcaae7464248cb77cc56eeaec1c47
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-11 16:44:59 +0100

    refactor: remove Announcement and Header from cart page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,13 +1,9 @@
 import styles from './Cart.module.css';
-import Header from '../../components/Header/Header';
-import Announcement from '../../components/Announcement/Announcement';
 import ProductPage from '../../components/ProductPage/ProductPage';
 
 const Cart = () => {
   return (
     <div>
-      <Announcement text={'welcome to cart'} />
-      <Header />
       <main className={styles.main}>
         <h1 className={styles.title}>Cart</h1>
         <ul className={styles.cart}>
