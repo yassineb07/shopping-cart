b8131582b52856822505da750f27eb739dd92f98
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-17 17:28:43 +0100

    refactor: update ProductCard component to use props

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,11 +1,11 @@
 import styles from './ProductCard.module.css';
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
   return (
-    <div className={styles.card}>
-      <img src="" alt="" className={styles.img} />
-      <p className={styles.title}>title</p>
-      <p className={styles.price}>9.99 $</p>
+    <div className={styles.card} key={product.id}>
+      <img src={product.img} alt="product image" className={styles.img} />
+      <p className={styles.title}>{product.name}</p>
+      <p className={styles.price}>{product.price}</p>
     </div>
   );
 };
