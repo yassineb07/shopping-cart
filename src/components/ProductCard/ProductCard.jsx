4167d89316ddc06a17ddeb771d2b337b79627d3c
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-17 18:24:48 +0100

    refactore: Update ProductCard component and styles

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,9 +3,9 @@ import styles from './ProductCard.module.css';
 const ProductCard = ({ product }) => {
   return (
     <div className={styles.card} key={product.id}>
-      <img src={product.img} alt="product image" className={styles.img} />
-      <p className={styles.title}>{product.name}</p>
-      <p className={styles.price}>{product.price}</p>
+      <img src={product.image} alt="product image" className={styles.img} />
+      <p className={styles.title}>{product.title}</p>
+      <p className={styles.price}>${product.price}</p>
     </div>
   );
 };
