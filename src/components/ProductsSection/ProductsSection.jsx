3516f26ccfd66544479b6c3c90a5baf673951536
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-17 17:33:33 +0100

    refactor: update ProductsSection to accept dynamic data

--- src/components/ProductsSection/ProductsSection.jsx
+++ src/components/ProductsSection/ProductsSection.jsx
@@ -1,15 +1,11 @@
 import styles from './ProductsSection.module.css';
 import ProductCard from '../ProductCard/ProductCard';
 
-const ProductsSection = () => {
-  return (
-    <section className={styles.products}>
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-    </section>
-  );
+const ProductsSection = ({ products }) => {
+  const productsList = products.map((product) => {
+    return <ProductCard product={product} />;
+  });
+  return <section className={styles.products}>{productsList}</section>;
 };
 
 export default ProductsSection;
