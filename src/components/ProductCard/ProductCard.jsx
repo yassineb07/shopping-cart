e2a0ddc7127e997410e5ab5d353285a1addd7d5d
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-18 13:44:44 +0100

    feat: add add to cart button to ProductCard component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -6,6 +6,7 @@ const ProductCard = ({ product }) => {
       <img src={product.image} alt="product image" className={styles.img} />
       <p className={styles.title}>{product.title}</p>
       <p className={styles.price}>${product.price}</p>
+      <button className={styles.add}>Add to cart</button>
     </div>
   );
 };
