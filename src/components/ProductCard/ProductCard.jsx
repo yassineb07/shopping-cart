d940486b4dd0de9bdbf4e678138b0de8825597e0
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-21 10:31:10 +0100

    refactor: remove key prop from ProductCard component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,7 +2,7 @@ import styles from './ProductCard.module.css';
 
 const ProductCard = ({ product }) => {
   return (
-    <div className={styles.card} key={product.id}>
+    <div className={styles.card}>
       <img src={product.image} alt="product image" className={styles.img} />
       <p className={styles.title}>{product.title}</p>
       <p className={styles.price}>${product.price}</p>
