f40142a69a240e9e7e592fe37d27fc994bfb410b
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-22 12:07:35 +0100

    refactor: remove data fetching from store page

--- src/pages/Store/Store.jsx
+++ src/pages/Store/Store.jsx
@@ -1,39 +1,15 @@
 import styles from './Store.module.css';
 import ProductsSection from '../../components/ProductsSection/ProductsSection';
-import { useState, useEffect } from 'react';
-import fetchProductData from '../../api';
+import { useOutletContext } from 'react-router';
 
 const Store = () => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const getProductsData = async () => {
-      try {
-        const productsData = await fetchProductData(
-          'https://fakestoreapi.com/products'
-        );
-        setData(productsData);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        setData(null);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getProductsData();
-  }, []);
+  const { data } = useOutletContext();
 
   return (
     <div>
       <section className={styles.products}>
         <h1 className={styles.title}>Products</h1>
-        {loading && <div className={styles.loading}>Loading products...</div>}
-        {error && <div className={styles.error}>{error}</div>}
-        {data && <ProductsSection products={data} />}
+        <ProductsSection products={data} />
       </section>
     </div>
   );
