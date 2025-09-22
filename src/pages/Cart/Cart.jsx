897faa85b328633126004df9624e3a681c86b917
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-22 12:20:04 +0100

    refactor: display cart state items in Cart page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,22 +1,20 @@
 import styles from './Cart.module.css';
 import ProductPage from '../../components/ProductPage/ProductPage';
+import { useOutletContext } from 'react-router';
 
 const Cart = () => {
+  const { cartState } = useOutletContext();
+  const [cart] = cartState;
+
+  const cartItems = cart.map((product) => {
+    return <ProductPage product={product} key={product.id} />;
+  });
+
   return (
     <div>
       <main className={styles.main}>
         <h1 className={styles.title}>Cart</h1>
-        <ul className={styles.cart}>
-          <li>
-            <ProductPage />
-          </li>
-          <li>
-            <ProductPage />
-          </li>
-          <li>
-            <ProductPage />
-          </li>
-        </ul>
+        <ul className={styles.cart}>{cartItems}</ul>
       </main>
     </div>
   );
