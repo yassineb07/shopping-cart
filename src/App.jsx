f3e7de2b59e17d0dd29428e562bf8cf89f1ff934
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-09-22 12:10:16 +0100

    refactor: lift data fetching to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,40 @@ import { Outlet } from 'react-router';
 import styles from './App.module.css';
 import Announcement from './components/Announcement/Announcement';
 import Header from './components/Header/Header';
+import { useState, useEffect } from 'react';
+import fetchProductData from './api';
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getProductsData = async () => {
+      try {
+        const productsData = await fetchProductData(
+          'https://fakestoreapi.com/products'
+        );
+        setData(productsData);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getProductsData();
+  }, []);
+
   return (
     <div>
       <Announcement text={'welcome to App'} />
       <Header />
-      <Outlet />
+      {loading && <div className={styles.loading}>Loading data...</div>}
+      {error && <div className={styles.error}>{error}</div>}
+      {data && <Outlet context={{ data }} />}
     </div>
   );
 };
